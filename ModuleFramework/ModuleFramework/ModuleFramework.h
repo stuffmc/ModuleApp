1ca52511ad3094067df10314620543e22d51a9c4
StuFF mc <m+git@carrascomolina.com>
2018-03-01 11:16:18 +0100

    Adding an ObjC Framework to the Repository

---
+++ ModuleFramework/ModuleFramework/ModuleFramework.h
@@ -0,0 +1,19 @@
+//
+//  ModuleFramework.h
+//  ModuleFramework
+//
+//  Created by StuFF mc on 01.03.18.
+//  Copyright © 2018 StuFF mc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleFramework.
+FOUNDATION_EXPORT double ModuleFrameworkVersionNumber;
+
+//! Project version string for ModuleFramework.
+FOUNDATION_EXPORT const unsigned char ModuleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleFramework/PublicHeader.h>
+
+
