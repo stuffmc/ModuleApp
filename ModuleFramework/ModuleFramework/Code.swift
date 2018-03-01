09afa66d7ad83fd0086324af00ad274df1204717
StuFF mc <m+git@carrascomolina.com>
2018-03-01 12:14:55 +0100

    Adding a (probably bad) usage of module.modulemap

--- ModuleFramework/ModuleFramework/Code.swift
+++ ModuleFramework/ModuleFramework/Code.swift
@@ -1,5 +1,6 @@
 extension Code {
     public func publicSwiftFunc() {
+        // I'm most probably doing something wrong the `module.modulemap` which seems not be loaded.
         print("How to call `privateObjCMethod` from here?")
     }
 }
