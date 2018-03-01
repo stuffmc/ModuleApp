4f8a16a394aba0ba82a044e7c31e806dfbd734bf
StuFF mc <m+git@carrascomolina.com>
2018-03-01 11:25:50 +0100

    Adding base for testing private and public methods

---
+++ ModuleFramework/ModuleFramework/Code.m
@@ -0,0 +1,19 @@
+//
+//  Created by StuFF mc on 01.03.18.
+//  Copyright © 2018 StuFF mc. All rights reserved.
+//
+
+#import "Code.h"
+#import "Code-Private.h"
+
+@implementation Code: NSObject
+
+- (void)publicMethod {
+    printf("PUBLIC");
+}
+
+- (void)privateMethod {
+    printf("PRIVATE");
+}
+
+@end
